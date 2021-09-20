9d162b790db0e10d4e32e5b94467e06073e46198
Peter Fontana <dude0508@gmail.com>
2021-09-20 18:54:02 -0400

    commit tree 2bb9c07400dae0025c33d5266e41f04c9d12a5e5

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+function saturdayFun(activity) {
+    if (typeof activity === 'string') {
+        return `This Saturday, I want to ${activity}!`;
+    } else {
+        return 'This Saturday, I want to roller-skate!';
+    }
+}
+
+saturdayFun('bathe my dog');
+
+function mondayWork(plan) {
+    switch (plan) {
+        case 'work from home':
+            return 'This Monday, I will work from home.';
+            break;
+        default:
+            return 'This Monday, I will go to the office.'
+    }
+}
+
+mondayWork('work from home');
+
+function wrapAdjective(flair) {
+    const text1 = 'You are ';
+    if (typeof flair === 'string') {
+        flair = flair;
+    } else {
+        flair = '*';
+    }
+    return function (input) {
+        return `${text1}${flair}${input}${flair}!`
+    }
+}
